c667484204f7191b58613d3e9e877f970ef55245
Martin Mazur <m.m@upplopp.se>
2020-12-03 11:05:23 +0100

    Added Solution for Day03 Part1

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 
 mod day01;
 mod day02;
+mod day03;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,6 +12,7 @@ fn main() {
     match day {
         1 => day01::run(),
         2 => day02::run(),
+        3 => day03::run(),
         _ => panic!()
     }
 }
