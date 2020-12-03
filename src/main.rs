ecb16612b12bab88c07c2edb6c57207a86ee9e90
Martin Mazur <m.m@upplopp.se>
2020-12-03 09:36:48 +0100

    Added parameter to switch between days

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use std::env;
+
 mod day01;
 mod day02;
 
 fn main() {
-    day02::run();
+    let args: Vec<String> = env::args().collect();
+    let day: i32 = args[1].parse()
+                            .expect("blah blah");
+
+    match day {
+        1 => day01::run(),
+        2 => day02::run(),
+        _ => panic!()
+    }
 }
