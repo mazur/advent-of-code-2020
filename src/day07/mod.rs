99e5d3c6e8838aae99355336846f513ca17da481
Martin Mazur <m.m@upplopp.se>
2020-12-09 23:47:24 +0100

    Solved Day07 Part2

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -4,18 +4,31 @@ use itertools::Itertools;
 pub fn run() {
     let system = BagSystem::build_from_rules(include_str!("input.txt"));
     let can_contain_shiny_gold = system.count_contains("shiny gold");
+    let shiny_gold_requires_contain = system.count_required_bags("shiny gold");
 
     println!("Day07 - Part 1: {}", can_contain_shiny_gold);
+    println!("Day07 - Part 2: {}", shiny_gold_requires_contain);
 }
 
 struct BagSystem<'a> {
-    bags: HashMap<&'a str, Vec<&'a str>>
+    contains: HashMap<&'a str, Vec<(&'a str, u32)>>,
+    contained_in: HashMap<&'a str, Vec<&'a str>>
 }
 
 impl<'a> BagSystem<'a> {
     fn new() -> Self {
         Self {  
-            bags: HashMap::new()
+            contains: HashMap::new(),
+            contained_in: HashMap::new()
+        }
+    }
+
+    fn count_required_bags(&self, key: &str) -> u32 {
+        match self.contains.get(key) {
+            Some (b) => {
+                b.iter().fold(0, |sum, bag| sum + bag.1 + (bag.1* self.count_required_bags(bag.0)))
+            },
+            None => 0
         }
     }
 
@@ -27,7 +40,7 @@ impl<'a> BagSystem<'a> {
     }
 
     fn get_all_contains(&self, key: &str) -> Vec<&str> {
-        match self.bags.get(key) {
+        match self.contained_in.get(key) {
             Some(b) => {
                 let mut res = b.to_vec();
                 for contained in b {
@@ -52,24 +65,30 @@ impl<'a> BagSystem<'a> {
     fn add_bag_rule(&mut self, rule_str: &'a str) {
         let (bag, rules) = rule_str.split(" bags contain ").collect_tuple().unwrap();
 
-        if rules != "no other bags." {
-            let contained_bags: Vec<&str> = rules.split(", ").map(|c| { 
-                let end = c.find(" bag").unwrap();
-                &c[2..end]
-            }).collect();
-            
-            self.add_bag(bag, contained_bags);
-        }
+        let contained_bags: Vec<(&str, u32)> = if rules != "no other bags." {
+                                                    rules.split(", ").map(|c| { 
+                                                        let end = c.find(" bag").unwrap();
+                                                        let n = c[0..1].parse().expect("Not a number");
+                                                        (&c[2..end], n)
+                                                    }).collect()
+                                                }
+                                                else {
+                                                    Vec::new()
+                                                };
+
+        self.add_bag(bag, contained_bags);
     }
 
-    fn add_bag(&mut self, bag: &'a str, contained_in: Vec<&'a str>) {
-        for con_bag in contained_in {
-            let contains = self.bags
-                                .entry(con_bag)
+    fn add_bag(&mut self, bag: &'a str, contained: Vec<(&'a str, u32)>) {
+        for con_bag in &contained {
+            let contained_in = self.contained_in
+                                .entry(con_bag.0)
                                 .or_insert(Vec::new());
             
-            contains.push(bag);
+            contained_in.push(bag);
         }
+
+        self.contains.insert(bag, contained);
     }
 }
 
@@ -90,12 +109,12 @@ dotted black bags contain no other bags.";
     #[test]
     fn test_add_bag_to_system() {
         let mut system = BagSystem::new();
-        let bags = vec!["shiny magenta", "wavy teal"];
+        let bags = vec![("shiny magenta",1), ("wavy teal",2)];
 
 
         system.add_bag("light red", bags);
 
-        assert_eq!(2, system.bags.len());
+        assert_eq!(2, system.contained_in.len());
     }
 
     #[test]
@@ -104,7 +123,7 @@ dotted black bags contain no other bags.";
 
         system.add_bag_rule("striped tomato bags contain no other bags.");
 
-        assert_eq!(0, system.bags.len());
+        assert_eq!(0, system.contained_in.len());
     }
 
     #[test]
@@ -113,14 +132,14 @@ dotted black bags contain no other bags.";
 
         system.add_bag_rule("dark orange bags contain 3 bright white bags, 4 muted yellow bags.");
 
-        assert_eq!(2, system.bags.len());
+        assert_eq!(2, system.contained_in.len());
     }
 
     #[test]
     fn test_build_rule_to_system_from_input() {
         let system = BagSystem::build_from_rules(TEST_INPUT);
 
-        assert_eq!(7, system.bags.len());
+        assert_eq!(7, system.contained_in.len());
     }
 
     #[test]
@@ -139,5 +158,14 @@ dotted black bags contain no other bags.";
         let res = system.count_contains("shiny gold");
 
         assert_eq!(4, res);
+    }
+
+    #[test]
+    fn test_count_required_bags() {
+        let system = BagSystem::build_from_rules(TEST_INPUT);
+
+        let res = system.count_required_bags("shiny gold");
+
+        assert_eq!(32, res);
     }
 }
