ef5246ea9e94cec943235662721119b358719e6c
Martin Mazur <m.m@upplopp.se>
2020-12-19 10:04:03 +0100

    Not the solution. Went down a strange rabbit hole.

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -2,18 +2,33 @@ use itertools::Itertools;
 
 pub fn run() {
     let input = include_str!("input.txt");
-    let (ruleset, _, tickets) = parse_input(input);
+    let (ruleset, ticket, mut tickets) = parse_input(input);
 
-    let errors = ruleset.find_errors(tickets)
+    let errors = ruleset.find_errors(&tickets)
                         .iter()
                         .fold(0, |sum, i| sum + i);
 
     println!("Day16 - Part 1: {}", errors);
+
+    remove_invalid_tickets(&mut tickets, &ruleset);
+
+    let dep_rules = ruleset.split(vec!["departure location", "departure station", "departure platform", "departure track", "departure date", "departure time"]);
+
+    let fields = dep_rules.find_fields(tickets);
+
+    for x in fields {
+        println!("{}", x);
+    }
+
+    //let res = fields.iter().fold(1, |prod, i| prod * ticket[*i]);
+
+    //println!("Day16 - Part 2: {}", res);
 }
 
 type Ticket = Vec<u32>;
 type Range = (u32, u32);
 
+#[derive(Clone)]
 struct Rule {
     id: String,
     ranges: Vec<Range>
@@ -48,7 +63,7 @@ struct RuleSet {
     rules: Vec<Rule>
 }
 
-impl RuleSet {
+impl RuleSet {    
     fn from(rule_str: &str) -> Self {
         let rules = rule_str.lines().map(|l| Rule::from(l)).collect();
 
@@ -66,11 +81,35 @@ impl RuleSet {
         false
     }
 
-    fn find_errors(&self, tickets: Vec<Ticket>) -> Vec<u32> {
+    fn find_errors(&self, tickets: &Vec<Ticket>) -> Vec<u32> {
         tickets.iter().flatten()
                 .filter(|&i| !self.match_rule(*i))
                 .map(|&i| i)
                 .collect()
+    }
+
+    fn split(&self, rule_id: Vec<&str>) -> Self {
+        RuleSet {
+            rules: self.rules.iter().filter(|r| rule_id.contains(&r.id.as_str())).map(|i| i.clone()).collect()
+        }
+    }
+
+    fn find_fields(&self, tickets: Vec<Ticket>) -> Vec<usize> {
+        let by_field: Vec<Vec<u32>> = (0..tickets[0].len())
+                                            .map(|i| tickets.iter()
+                                                            .map(|t| t[i])
+                                                            .collect())
+                                            .collect();
+
+        let mut matched_fields = Vec::new();
+
+        for i in 0..by_field.len() {
+            if self.rules.iter().any(|r| by_field[i].iter().all(|n| r.match_rule(*n))) {
+                matched_fields.push(i);
+            }
+        }
+
+        matched_fields
     }
 }
 
@@ -84,6 +123,10 @@ fn parse_input(input: &str) -> (RuleSet, Ticket, Vec<Ticket>) {
     (RuleSet::from(rule_str), 
     parse_ticket(my_ticket_str.lines().nth(1).expect("Failed attempt to skip label")),
     tickets_str.lines().skip(1).map(|l| parse_ticket(l)).collect())
+}
+
+fn remove_invalid_tickets(tickets: &mut Vec<Ticket>, rules: &RuleSet) {
+    tickets.retain(|t| t.iter().all(|f| rules.match_rule(*f)));
 }
 
 #[cfg(test)]
@@ -149,8 +192,55 @@ nearby tickets:
 55,2,20
 38,6,12");
 
-        let errors = ruleset.find_errors(tickets);
+        let errors = ruleset.find_errors(&tickets);
 
         assert_eq!(71, errors.iter().fold(0, |sum, i| sum + i));
+    }
+
+    #[test]
+    fn test_find_fields() {
+        let (ruleset, _, tickets) = parse_input("class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9");
+
+        let ids = ruleset.find_fields(tickets);
+
+        assert_eq!(vec![0,1,2], ids);
+    }
+
+    #[test]
+    fn test_split_ruleset() {
+        let ruleset = RuleSet::from("departure location: 40-152 or 161-969\ndeparture station: 39-838 or 845-971\ndeparture platform: 39-209 or 217-970");
+        let split = ruleset.split(vec!["departure station", "ndeparture platform"]);
+
+        assert_eq!(1, split.rules.len());
+    }
+
+    #[test]
+    fn test_remove_invalid_tickets() {
+        let (ruleset, _, mut tickets) = parse_input("class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12");
+
+        remove_invalid_tickets(&mut tickets, &ruleset);
+
+        assert_eq!(1, tickets.len());
     }
 }
